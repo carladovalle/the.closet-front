1858f6b46cc7aedbe4186269c1f2059228014136
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-14 13:18:19 -0300

    feat(layout header menu) - Implementação do layout da barra superior (logo e toggle menu)

--- src/Components/Menus/HeaderMenu.js
+++ src/Components/Menus/HeaderMenu.js
@@ -1,12 +1,21 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
 import styled from 'styled-components';
+import logo from '../../assets/logo.svg';
 
 export default function HeaderMenu() {
-  return <Header />;
+  return (
+    <Header>
+      <ion-icon name="reorder-four-outline" />
+      <img src={logo} alt="logo" />
+    </Header>
+  );
 }
 
 const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 50px;
   width: 100vw;
   background-color: #ffffff;
@@ -14,4 +23,11 @@ const Header = styled.div`
   top: 0;
   left: 0;
   border-bottom: 1px solid #c3c3c3;
+
+  ion-icon {
+    font-size: 22px;
+    color: #5b3e40;
+    position: absolute;
+    left: 20px;
+  }
 `;

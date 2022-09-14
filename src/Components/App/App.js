ac8e0e93e7ee50a38e38f6ebedcd6426ed6bce73
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-14 00:56:37 -0300

    feat(reset css e global style) - Implementação do reset css e do global style

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import { GlobalStyle, Reset } from '../../Common/globalStyle';
+
 export default function App() {
   return (
     <>
+      <Reset />
+      <GlobalStyle />
+      <h1>Hello world!</h1>
     </>
   );
 }

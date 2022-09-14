829748a0921462b1282bef7e00e06bbabd146189
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-14 03:14:41 -0300

    feat(armazenamento dados cadastro) - Implementada a lógica para armazenamento e verificação dos dados de cadastro, com envio ao servidor, via axios

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,15 +1,59 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-no-bind */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
+import axios from 'axios';
+import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function SignUp() {
+  const [signupData, setSignupData] = useState({});
+  // const navigate = useNavigate();
+
+  function handleForm(e) {
+    setSignupData({
+      ...signupData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function sendForm(e) {
+    e.preventDefault();
+    const { user, email, password, confirmPassword } = signupData;
+
+    if (!user || !email || !password) {
+      alert(
+        'Todos os campos são de preenchimento obrigatório!\nPor gentileza, revise seus dados!'
+      );
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert(
+        'As senhas inseridas devem ser iguais!\nPor gentileza, revise seus dados!'
+      );
+      return;
+    }
+
+    try {
+      await axios.post('http://localhost:5000/signup', signupData);
+      alert('Usuário criado com sucesso! :)');
+      // navigate('/login');
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
   return (
     <Wrapper>
       <LoginContainer>
         <ion-icon name="shirt-outline" />
         <h1>Crie sua conta!</h1>
-        <Form>
+        <Form onSubmit={sendForm}>
           <section>
             <label htmlFor="user">Nome de usuário:</label>
             <input
@@ -17,7 +61,7 @@ export default function SignUp() {
               name="user"
               id="user"
               placeholder="Digite seu username"
-              required
+              onChange={handleForm}
             />
           </section>
           <section>
@@ -27,7 +71,7 @@ export default function SignUp() {
               name="email"
               id="email"
               placeholder="Digite seu e-mail"
-              required
+              onChange={handleForm}
             />
           </section>
           <section>
@@ -37,7 +81,7 @@ export default function SignUp() {
               name="password"
               id="password"
               placeholder="Digite sua senha"
-              required
+              onChange={handleForm}
             />
           </section>
           <section>
@@ -47,10 +91,10 @@ export default function SignUp() {
               name="confirmPassword"
               id="confirmPassword"
               placeholder="Confirme a senha digitada anteriormente"
-              required
+              onChange={handleForm}
             />
           </section>
-          <button type="submit">Cadastrar</button>
+          <button>Cadastrar</button>
         </Form>
       </LoginContainer>
       <span>Já possui um usuário? Faça já seu login!</span>
@@ -99,7 +143,7 @@ const LoginContainer = styled.div`
   background-color: #ffffff;
 `;
 
-const Form = styled.main`
+const Form = styled.form`
   width: 100%;
   display: flex;
   flex-direction: column;
